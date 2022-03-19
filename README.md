6a0b6f5768381a63378648ed81a9a33e935e9f3a
rider150 <rider150@gmail.com>
2022-03-19 18:41:48 +0100

    initialize

---
+++ README.md
@@ -0,0 +1,38 @@
+# Cart
+
+The purpose of this exercise is to develop simple cart & promotion engine.
+
+## Your tasks
+
+
+Test classes must not be modified !
+
+Implement missing part of Cart class and Services
+
+
+## Tips
+
+The service CartStorage should not be modified.
+
+Fix unit tests one by one with this proposed order :
+
+* CartTest
+* CartServiceTest
+* PromotionServiceTest
+* IntegrationTest
+
+
+## How to submit 
+
+Fork the repository .
+
+Fix each of the tests.
+
+create a pull request to the repository .
+
+branch name should follow the following format feature/firstName_lastName (ex .: feature/younes_elhaiti)
+
+### Submit tips 
+* Keep the commits small and meaningful
+* Continuous commits
+* Have a clear commit message
---
+++ src/main/java/com/nespresso/sofa/interview/cart/services/CartService.java
@@ -0,0 +1,96 @@
+package com.nespresso.sofa.interview.cart.services;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+import com.nespresso.sofa.interview.cart.model.Cart;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CartService {
+
+    @Autowired
+    private PromotionEngine promotionEngine;
+    @Autowired
+    private CartStorage cartStorage;
+
+    Logger logger = Logger.getLogger(this.getClass().getName());
+
+    /**
+     * Add a quantity of a product to the cart and store the cart
+     *
+     * @param cartId
+     *     The cart ID
+     * @param productCode
+     *     The product code
+     * @param quantity
+     *     Quantity must be added
+     * @return True if card has been modified
+     */
+    public boolean add(UUID cartId, String productCode, int quantity) {
+        boolean isOperationSuccessful = false;
+        final Cart cart = cartStorage.loadCart(cartId);
+        final Map<String, Integer> products = cart.getProducts();
+
+        if(quantity > 0){
+            products.merge(productCode, quantity, Integer::sum);
+            isOperationSuccessful = true;
+        }else if(quantity < 0){
+            int oldProductQuantity = products.getOrDefault(productCode,0);
+            int difference = oldProductQuantity + quantity;
+            if(difference > 0){
+                products.put(productCode,difference);
+                isOperationSuccessful = true;
+            }
+        }
+
+        if(isOperationSuccessful){
+            Cart cartAfterPromotions = promotionEngine.apply(cart);
+            cartStorage.saveCart(cartAfterPromotions);
+            logger.info("Product add: " + cartAfterPromotions.getProducts());
+        }
+
+        return isOperationSuccessful;
+    }
+
+    /**
+     * Set a quantity of a product to the cart and store the cart
+     *
+     * @param cartId
+     *     The cart ID
+     * @param productCode
+     *     The product code
+     * @param quantity
+     *     The new quantity
+     * @return True if card has been modified
+     */
+    public boolean set(UUID cartId, String productCode, int quantity) {
+        final Cart cart = cartStorage.loadCart(cartId);
+        final Map<String,Integer> products = cart.getProducts();
+
+        if(quantity <= 0){
+            products.remove(productCode);
+        }else{
+            Integer oldQuantity = products.getOrDefault(productCode,0);
+            if(oldQuantity == quantity){
+                return false;
+            }
+            products.put(productCode, quantity);
+        }
+
+        Cart cartAfterPromotions = promotionEngine.apply(cart);
+        cartStorage.saveCart(cartAfterPromotions);
+        logger.info("Product set: " + cartAfterPromotions.getProducts());
+        return true;
+    }
+
+    /**
+     * Return the card with the corresponding ID
+     *
+     * @param cartId
+     * @return Cart
+     */
+    public Cart get(UUID cartId) {
+        return cartStorage.loadCart(cartId);
+    }
+}
